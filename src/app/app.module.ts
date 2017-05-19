d34c8362bd86c917938b5b0764ea15cc505fbccc
Miroslav Novak <miroslav.novak@gmail.com>
2017-05-19 13:37:05 +0200

    adding error handler (not used yet)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule, ErrorHandler} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -7,19 +7,22 @@ import { AppComponent } from './app.component';
 import { AppFormComponent } from './form/app-form.component';
 import {AppService} from "./service/app-service";
 import { ValidatePersonalId } from './form/validate.personal.id';
+import {AppErrorHandler} from "./error/error-handler";
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppFormComponent,
-    ValidatePersonalId
+    ValidatePersonalId,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [AppService, ValidatePersonalId],
+  providers: [AppService, ValidatePersonalId, {provide: ErrorHandler, useClass: AppErrorHandler}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
